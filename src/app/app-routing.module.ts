c6a09cbacf33e34acd9b693e9db293e525fa4d9f
Igor Smirnov <uchetzapis@yahoo.com>
2023-07-27 15:44:04 +0300

    Make router work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainPageComponent,
+  },
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: 'profile', component: ProfileComponent,
+  },
+  {
+    path: 'ticket', component: TicketListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LoginComponent,
+    ProfileComponent,
+    TicketListComponent
   ],
   imports: [
     BrowserModule,
